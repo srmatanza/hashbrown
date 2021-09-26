a68f55d8127f13d0772a500459474ef117bb43d4
Mike McEuen <srmatanza@gmail.com>
2021-09-26 01:16:02 -0400

    Adding a comment to reflect my ignorance of configuring the basic net-server.

--- main.go
+++ main.go
@@ -28,6 +28,12 @@ func main() {
   datastore.Shutdown()
 }
 
+// Serve will setup our basic listener. Apparently TCP keep-alives are enabled by default
+// when we use this convenience method, and we probably don't want that for a basic REST
+// server. It's probably necessary to spend some time analyzing the requirements of this
+// service to determine what our exposure to DDoS attacks is here.
+//
+// To-do, spend some time getting familiar with the underlying net library.
 func Serve() {
   err := http.ListenAndServe(*addr, nil)
   if err != nil {
