757d9006601c062064b613b41e30c35e9e58c1d9
Mike McEuen <srmatanza@gmail.com>
2021-10-02 20:51:15 -0400

    Fix up the three_hash.sh test program to show the stats before sleeping.

--- test/three_hash.sh
+++ test/three_hash.sh
@@ -7,6 +7,7 @@ curl -X POST 127.0.0.1:8123/hash --data "password=password"
 curl 127.0.0.1:8123/hash/1
 curl 127.0.0.1:8123/hash/2
 curl 127.0.0.1:8123/hash/3
+curl 127.0.0.1:8123/stats
 sleep 5
 
 curl 127.0.0.1:8123/hash/1
